da10ee2ecb0e879e4c49786b460993324174cb04
TroyMena <troymena@gmail.com>
2023-10-21 16:52:49 -0400

    I added the letters variable and created an array with the letters of the alphabet. I also put in console.log(letters.length) to confirm I have the 26 alphabet characters in the array.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// Just to confirm there are 26 letters!
+console.log(letters.length);
 
 // Write password to the #password input
 function writePassword() {
