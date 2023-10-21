afb0f128b888397917a97a23b0f64c3cf5248b6e
TroyMena <troymena@gmail.com>
2023-10-21 19:40:43 -0400

    I realized changing the readonly value to false for the text box is unnecessary, as that is where the generated password would appear. I still may need the cardBody variable defined, but I commented out the line where the readonly attribute's value is changed to false.

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var cardBody = document.querySelector(".card-body");
 
 cardBody.addEventListener("click",function() {
-	document.getElementById('password').readOnly = false;
+//	document.getElementById('password').readOnly = false;
 })
 
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
