d13ca9bb15398500f32da3a6500fff3732cd8d2d
TroyMena <troymena@gmail.com>
2023-10-21 19:36:02 -0400

    I defined the cardBody variable, which targets the card-body class. I also added an event listener to cardBody, where upon a click, the readonly attribute will be disabled because a boolean of false was added to it. I saw on a StackOverflow question regarding how to remove the readonly attribute that it needs to be listed in camelcase when changing its value to false. Because of this, now I can type in the text box, and the text displaying 'Your Secure Password' disappears upon typing anything.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var cardBody = document.querySelector(".card-body");
+
+cardBody.addEventListener("click",function() {
+	document.getElementById('password').readOnly = false;
+})
+
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 // Just to confirm there are 26 letters!
 console.log(letters.length);
