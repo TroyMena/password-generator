905bbc3894dcff459eda7605f84e342ae6864462
TroyMena <troymena@gmail.com>
2023-10-23 23:52:17 -0400

    Added comments describing the code. Updated README file. Included a screenshot of the website at the time of writing the README file. Deleted  demo png.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// I am first starting this password generator out with a pre-fixed length of 8 characters
 var charLimit = 8;
 
+// The letters variable is an array of all lowercase alphabetical characters.
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  // The console.log(password) below was to confirm the button returned a response when clicked.
   // console.log("test");
   var passwordText = document.querySelector("#password");
 
@@ -20,8 +23,11 @@ function generatePassword () {
   var password = ""
   for(var i = 0; i < charLimit; i = i + 1) {
     password = password += letters[Math.floor(Math.random() * letters.length)];
-    console.log(password);
+    // The console.log(password) below was to confirm the characters were randomly selected and that each character was appended to the variable, password, after each iteration (until there were 8 iterations).
+    // console.log(password);
   }
+
+  // To link the generatePassword() function to the writePassword() function, it needs the return below.
   return password;
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # password-generator
-Generates a password of at least 8 characters and no more than 128 characters
+Generates a password of at least 8 characters and no more than 128 characters.
+
+Currently, it returns a generated password of random lowercase letters with a length of 8 characters. This will be improved to then have a combination of lowercase letters, uppercase letters, numbers, and special characters; with the user being able to choose any number between 8 and 120 characters for their generated password to have.
+
+## Website URL
+
+https://troymena.github.io/password-generator/
+
+## Website Screenshot
+
+The screenshot below shows the website at the time of writing this README file:
+
+![website screenshot](./assets/images/website-screenshot.png)
