3a416d40a9d6a64feb5a5f390b4c10faaf560554
TroyMena <troymena@gmail.com>
2023-10-23 23:33:33 -0400

    Kept the passwordText variable as a local scope of the writePassword() function. Defined and executed the generatePassword() function. To confirm the function was working, I added console.log(password) to confirm each character was getting appended to the empty string variable, password. I also added the return statement to the end of the generatePassword() function.

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password")
 
-var length = 8;
+var charLimit = 8;
 
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-// Just to confirm there are 26 letters!
-console.log(letters.length);
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword(letters);
-  var password = ""
+  var password = generatePassword();
+  // console.log("test");
   var passwordText = document.querySelector("#password");
-  for(var i = 0; i < 9; i = i + 1) {
-
-  }
 
   passwordText.value = password;
+}
 
+
+
+function generatePassword () {
+  var password = ""
+  for(var i = 0; i < charLimit; i = i + 1) {
+    password = password += letters[Math.floor(Math.random() * letters.length)];
+    console.log(password);
+  }
+  return password;
 }
 
 // Add event listener to generate button
